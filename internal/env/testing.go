64045fadfbb79e664520a8f927abf4119b7136b6
Colin Hoglund <colinhoglund@gmail.com>
2022-08-16 11:44:57 -0700

    add comment

--- internal/env/testing.go
+++ internal/env/testing.go
@@ -7,6 +7,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+// NewTestConfig is a helper for setting up a Config object and error checking
 func NewTestConfig(t *testing.T) *Config {
 	conf, err := NewConfig(os.Stdout, os.Stderr)
 	require.NoError(t, err)
